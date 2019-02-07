0312a252b5eb393c2f867d302efc8930119b0d5b
EB21 <ebaraja1@mail.sfsu.edu>
2019-02-06 20:27:59 -0800

    updated functions.js

--- node/functions.js
+++ node/functions.js
@@ -10,10 +10,19 @@ const arrow = (test) => {
   console.log(test);
 }
 
-arrow('hi')
+arrow('hi');
 
 // for practice
 
 // make an arrow function with no block body
-
+const myArrow = (y) => y + 1;
+// {
+//   return y + 1;
+// }
+console.log(myArrow(8));
 // make a function that returns a function
+const functionFactory = () => () => console.log('This is wierd');
+const a = functionFactory();
+a();
+const anotherFunction = functionFactory;
+anotherFunction()();
